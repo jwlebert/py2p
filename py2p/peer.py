3b4d33c74329f129486aab6e3a925b8eda404fe0
jwlebert <j.lebert4402@gmail.com>
2023-09-17 17:38:50 -0400

    got passing tests, need to write more tests

--- py2p/peer.py
+++ py2p/peer.py
@@ -1,94 +1,181 @@
 import socket
+import struct
 import random
 
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, List
+
+from py2p.listener import Listener
 
 class PeerInfo:
-	"""Represents the contact information of a peer on the network."""
-	def __init__(self, host: str = '127.0.0.1', port: int = None, addr: Tuple[str, int] = None):
-		RAND_UP, RAND_LOW = 12000, 12100
+    """Represents the contact information of a peer on the network."""
+    def __init__(self, host: str = '127.0.0.1', port: int = None, addr: Tuple[str, int] = None):
+        RAND_UP, RAND_LOW = 12000, 12500
 
-		if addr != None:
-			self.host = addr[0]
-			self.port = addr[1]
-			return
-		
-		self.host: str = host
-		self.port: int = port if port is not None else random.choice(range(RAND_UP, RAND_LOW))
-	
-	@property
-	def address(self) -> Tuple[str, int]:
-		return (self.host, self.port)
-	
+        if addr != None:
+            self.host = addr[0]
+            self.port = addr[1]
+            return
+        
+        self.host: str = host
+        self.port: int = port if port is not None else random.choice(range(RAND_UP, RAND_LOW))
+        self.owner: Peer = None
+    
+    @property
+    def address(self) -> Tuple[str, int]:
+        return (self.host, self.port)
+    
+    def __eq__(self, other):
+        if isinstance(other, PeerInfo):
+            return self.host == other.host and \
+                self.port == other.port and \
+                self.address == other.address
+        return NotImplemented
+    
+    # def __hash__(self) -> int:
+    #     return hash((self.host, self.port, self.address))
+
+    
 class PeerConnection:
-	def __init__(self, socket: socket.socket, info: PeerInfo) -> None:
-		self.socket: socket.socket = socket
-		self.info: PeerInfo = info
+    socket: socket.socket
+    info: PeerInfo
 
-	def send(self, data, command = 'READ') -> bool:
-		pass
+    def __init__(self) -> None:
+        self.data: socket.SocketIO = self.socket.makefile('rwb', 0)
 
-	def recv(self) -> Tuple[str, str]:
-		pass
-
-	def __pack_data(self, data):
-		pass
-
-	def __unpack_data(self, data):
-		pass
-
-	def close(self):
-		self.send("END OF TRANSMISSION")
-
-		self.socket.close()
+    def close(self):
+        self.socket.close()
 
 class IncomingConnection(PeerConnection):
-	def __init__(self, socket: socket.socket, info: PeerInfo) -> None:
-		PeerConnection.__init__(self, socket, info)
-	
-class OutgoingConnection(PeerConnection):
-	def __init__(self, info: PeerInfo) -> None:
-		self.info: PeerInfo = info
+    def __init__(self, sock: socket.socket) -> None:
+        self.socket: socket.socket = sock
+        self.info: PeerInfo = None
 
-		self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect(self.info.address)
+        PeerConnection.__init__(self)
+    
+    def recv(self) -> Tuple[str, str]:
+        try:
+            instruction = self.data.read(4).decode("utf-8")
+            if not instruction: raise ValueError("No instruction")
+            # check the type of message being sent (identifier)
+
+            data = self.data.read(4)
+            msg_len = struct.unpack("!I", data)[0]
+            if not msg_len: raise ValueError("No message length")
+
+            data = ""
+
+            while len(data) != msg_len:
+                d = self.data.read(min(2048, msg_len - len(data)))
+                # read either 2048 bytes or the remaining amount of data,
+                # whichever is smaller
+                if not len(d): break
+                data += d.decode("utf-8")
+            
+            if len(data) != msg_len:
+                raise Exception("length of data received is not same as supposed message length")
+            
+            len_bytes = self.data.read(4)
+            host_len = struct.unpack("!I", len_bytes)[0]
+            if host_len != 0:
+                host = self.data.read(host_len).decode('utf-8')
+                port_bytes = self.data.read(4)
+                port = struct.unpack("!I", port_bytes)[0]
+
+            if (host, port) != ('NO_RET_ADDR', 0):
+                self.info = PeerInfo(host=host, port=port)
+            else:
+                print("NO RETURN ADDRESS")
+
+        except Exception as e:
+            print(e)
+            return None
+        
+        return (instruction, data)
+    
+class OutgoingConnection(PeerConnection):
+    def __init__(self, info: PeerInfo) -> None:
+        self.info: PeerInfo = info
+
+        self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.info.address)
+
+        PeerConnection.__init__(self)
+    
+    def send(self, data, instruction = 'READ', ret_addr = None) -> bool:
+        try:
+            instruction = instruction.encode('utf-8')
+            msg = data.encode('utf-8')
+
+            host, port = 'NO_RET_ADDR', 0
+            if ret_addr is not None:
+                host, port = ret_addr
+                
+            host = host.encode('utf-8')
+
+            data = struct.pack("!4sI%dsI%dsI" % (len(msg), len(host)), instruction, len(msg), msg, len(host), host, port)
+            # "!4sL%ds" is the format that we are packing to
+            # !     ->  bite order, size, alignment
+            # 4s    ->  a string of 4 characters
+            # I     ->  an unsigned int (4 byte number, with no sign (+/-))
+            # %ds   ->  a string of length %d, where d is replaced by what follows
+            #           the '%' after the format (in this case, len(msg))
+            
+            self.data.write(data)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
 class Peer:
-	"""Represents a peer on the peer to peer network."""
-	alive: bool = True
-	listen_sock: socket.socket = None
+    """Represents a peer on the peer to peer network."""
+    alive: bool = True
+    listen_sock: socket.socket = None
+    transmissions: List[Tuple[PeerInfo, str, str]] = []
 
-	def __init__(self, info: PeerInfo = None) -> None:
-		self.info: PeerInfo = info if info is not None else PeerInfo()
-		self.start_listener()
+    def __init__(self, info: PeerInfo = None) -> None:
+        self.info: PeerInfo = info if info is not None else PeerInfo()
+        self.info.owner = self;
+        self.start_listener()
 
-	def start_listener(self, backlog: int = 5) -> None:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.bind(self.info.address)
-		s.listen(backlog)
+    def start_listener(self, backlog: int = 5, timeout: int = 5) -> None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(self.info.address)
+        s.listen(backlog)
 
-		self.listen_sock: socket.socket = s
+        self.listen_sock: socket.socket = s
 
-		self.listener: Thread = Thread(target = self.await_connection)
-		self.listener.start()
+        self.listener: Listener = Listener(target = self.await_connection)
+        self.listener.start()
 
-	def await_connection(self) -> None:
-		sock, addr = self.listen_sock.accept()
-		sock.settimeout(120)
+    def await_connection(self) -> None:
+        while self.alive:
+            sock, addr = self.listen_sock.accept()
 
-		info = PeerInfo(addr=addr)
+            connection = IncomingConnection(sock)
+            t = Thread(target = self.handle_connection, args = [connection])
+            t.start()
 
-		connection = IncomingConnection(sock, info)
+    def handle_connection(self, connection: IncomingConnection) -> None:
+        recv = connection.recv()
+        if recv == None:
+            raise
 
-		t = Thread(target = self.handle_connection, args = [connection])
+        # do stuff
 
-	def handle_connection(self, connection: IncomingConnection) -> None:
-		command, data = connection.recv()
-		
-	
-	def kill(self) -> None:
-		self.listen_sock.close()
+        print((connection.info, *recv))
 
-		self.alive = False
+        self.transmissions.append((connection.info, *recv))
+
+    def send_data(self, target: PeerInfo, data, instruction = 'READ') -> bool:
+        connection = OutgoingConnection(target)
+        return connection.send(data, instruction = instruction, ret_addr = self.info.address)
+    
+    def kill(self) -> None:
+        self.listener.close()
+        if self.listener.alive:
+            self.listener.join(5)
+        self.listen_sock.close()
+
+        self.alive = False
--- test/test_peers.py
+++ test/test_peers.py
@@ -1,34 +1,50 @@
 import pytest
 
 import socket
+import time
 
 from py2p.peer import Peer, PeerInfo
 
 def test_peer_creation():
-	peer = Peer()
+    peer = Peer()
 
-	assert peer
-	assert peer.alive == True
+    assert peer
+    assert peer.alive == True
 
-	peer.kill()
-	assert peer.alive == False
+    peer.kill()
+    assert peer.alive == False
 
-	info = PeerInfo(host='127.0.0.1', port=12001)
-	peer = Peer(info=info)
+    info = PeerInfo(host='127.0.0.1', port=12001)
+    peer = Peer(info=info)
 
-	assert peer.info == info
-	assert peer.info.host == info.host
-	assert peer.info.port == info.port
-	assert peer.info.address == info.address
+    assert peer.info == info
+    assert peer.info.host == info.host
+    assert peer.info.port == info.port
+    assert peer.info.address == info.address
 
-	peer.kill()
-	assert peer.alive == False
+    peer.kill()
+    assert peer.alive == False
 
 def test_listener_creation():
-	peer = Peer()
+    peer = Peer()
 
-	assert peer.listener is not None
-	assert peer.listener.type is socket.SOCK_STREAM
-	assert peer.listener.getsockname() == peer.info.address
+    assert peer.listen_sock is not None
+    assert peer.listen_sock.type is socket.SOCK_STREAM
+    assert peer.listen_sock.getsockname() == peer.info.address
 
-	peer.kill()
+    peer.kill()
+
+def test_connections():
+    peer1 = Peer()
+    peer2 = Peer()
+
+    assert peer1.send_data(peer2.info, "Hello there!")
+    
+    for _ in range(50): #time buffer for transmission
+        if len(peer2.transmissions) >= 1: break
+        time.sleep(0.1)
+    
+    assert (peer1.info, 'READ', "Hello there!") in peer2.transmissions
+
+    peer1.kill()
+    peer2.kill()
